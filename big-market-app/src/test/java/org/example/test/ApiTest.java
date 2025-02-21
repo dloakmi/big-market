a445e00c52a4780b7cf753a4e85dfbd393d2da4b
“dlokami” <1278963453@qq.com>
2025-02-21 18:11:58 +0800

    redis测试

--- big-market-app/src/test/java/org/example/test/ApiTest.java
+++ big-market-app/src/test/java/org/example/test/ApiTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.infrastructure.persistent.redis.IRedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,10 +14,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApiTest {
 
+    @Autowired
     private IRedisService redisService;
     @Test
     public void test() {
-        redisService.getMap("");
+        RMap<Object, Object> map = redisService.getMap("strategy_id_100001");
+        map.put(1,101);
+        map.put(2,102);
+        map.put(3,103);
+        map.put(4,104);
+        map.put(5,105);
+        map.put(6,106);
+        map.put(7,107);
+        log.info("测试结果: {}",redisService.getFromMap("strategy_id_100001", 1).toString() );
     }
 
 }
---
+++ big-market-domain/src/main/java/org/example/domain/strategy/service/armory/IStrategyArmory.java
@@ -0,0 +1,10 @@
+package org.example.domain.strategy.service.armory;
+
+/**
+ * @Author: 机智的赛尔
+ * @Description： 策略装配库（兵工厂），负责初始化策略计算
+ * @DateTime： 2025/2/21 18:10
+ **/
+
+public interface IStrategyArmory {
+}
