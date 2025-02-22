a5ce39c58d0aeb61b01ae7e7f360387cee1888b1
“dlokami” <1278963453@qq.com>
2025-02-22 16:14:53 +0800

    根据策略id进行装配，进行抽奖测试

--- big-market-domain/src/main/java/org/example/domain/strategy/repository/IStrategyRepository.java
+++ big-market-domain/src/main/java/org/example/domain/strategy/repository/IStrategyRepository.java
@@ -2,11 +2,13 @@ package org.example.domain.strategy.repository;
 
 import org.example.domain.strategy.model.entity.StrategyAwardEntity;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * @Author: 机智的赛尔
- * @Description： ****
+ * @Description：策略仓储接口
  * @DateTime： 2025/2/21 19:27
  **/
 
@@ -14,4 +16,10 @@ public interface IStrategyRepository {
 
 
     List<StrategyAwardEntity> queryStrategyAwardlist(Long strategyId);
+
+    void storeStrategyAwardSearchRateTables(Long strategyId, Integer rateRange, HashMap<Integer, Integer> shuffledStrategyAwardSearchRateTable);
+
+    int getRateRange(Long strategyId);
+
+    int getStrategyAwardAssemble(Long strategyId, int ratekey);
 }
