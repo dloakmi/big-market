8980d654dba11d56947d03b69889ae127baebc10
dloakmi <1278963453@qq.com>
2025-03-21 15:37:20 +0800

    第二阶段开始-建表和po类创建

---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/po/RaffleActivityAccount.java
@@ -0,0 +1,72 @@
+package org.example.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: 机智的赛尔
+ * @Description： 抽奖活动账户表 持久化对象
+ * @DateTime： 2025/3/21 15:28
+ **/
+
+@Data
+public class RaffleActivityAccount {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 总次数-剩余
+     */
+    private Integer totalCountSurplus;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 日次数-剩余
+     */
+    private Integer dayCountSurplus;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 月次数-剩余
+     */
+    private Integer monthCountSurplus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
+
---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/po/RaffleActivityOrder.java
@@ -0,0 +1,67 @@
+package org.example.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: 机智的赛尔
+ * @Description： 抽奖活动单 持久化对象
+ * @DateTime： 2025/3/21 15:30
+ **/
+
+@Data
+public class RaffleActivityOrder {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动名称
+     */
+    private String activityName;
+
+    /**
+     * 抽奖策略ID
+     */
+    private Long strategyId;
+
+    /**
+     * 订单ID
+     */
+    private String orderId;
+
+    /**
+     * 下单时间
+     */
+    private Date orderTime;
+
+    /**
+     * 订单状态（not_used、used、expire）
+     */
+    private String state;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
+
