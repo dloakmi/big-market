b3c1e2ccf0ac0aab97ff9a7ae89e6ad26cdbdbe0
dloakmi <1278963453@qq.com>
2025-03-28 18:02:44 +0800

    引入MQ处理活动SKU库存一致性-测试完成

---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IRaffleActivityAccountDayDao.java
@@ -0,0 +1,10 @@
+package org.example.infrastructure.persistent.dao;
+
+/**
+ * @Author: 机智的赛尔
+ * @Description： 抽奖活动账户表-日次数
+ * @DateTime： 2025/3/28 17:57
+ **/
+
+public interface IRaffleActivityAccountDayDao {
+}
---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/po/Task.java
@@ -0,0 +1,30 @@
+package org.example.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: 机智的赛尔
+ * @Description： 任务表，发送MQ
+ * @DateTime： 2025/3/28 17:54
+ **/
+
+@Data
+public class Task {
+
+    /** 自增ID */
+    private String id;
+    /** 消息主题 */
+    private String topic;
+    /** 消息主体 */
+    private String message;
+    /** 任务状态；create-创建、completed-完成、fail-失败 */
+    private String state;
+    /** 创建时间 */
+    private Date createTime;
+    /** 更新时间 */
+    private Date updateTime;
+
+
+}
