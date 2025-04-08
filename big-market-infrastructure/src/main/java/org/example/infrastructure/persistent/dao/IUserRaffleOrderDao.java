1c4195aa67f5a4b01aae05055ee01dfeed503fce
“dlokami” <1278963453@qq.com>
2025-04-08 17:33:47 +0800

    领取活动扣减额度-测试完成

--- big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IUserRaffleOrderDao.java
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IUserRaffleOrderDao.java
@@ -1,6 +1,7 @@
 package org.example.infrastructure.persistent.dao;
 
 import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
 import org.apache.ibatis.annotations.Mapper;
 import org.example.infrastructure.persistent.po.UserRaffleOrder;
 
@@ -11,6 +12,7 @@ import org.example.infrastructure.persistent.po.UserRaffleOrder;
  **/
 
 @Mapper
+@DBRouterStrategy(splitTable = true)
 public interface IUserRaffleOrderDao {
 
     void insert(UserRaffleOrder userRaffleOrder);
